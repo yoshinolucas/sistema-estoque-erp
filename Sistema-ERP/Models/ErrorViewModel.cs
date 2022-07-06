a22e3e4f521e8fbba5c6844d7ec00bd730b2a5ae
Lucas Yudi <lucas.yudi7@gmail.com>
2022-07-05 22:46:57 -0300

    Add project files.

---
+++ Sistema-ERP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema_ERP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
