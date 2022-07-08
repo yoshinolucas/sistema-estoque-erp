0afdb23de38346c3231c8b74564eed9e447a5f57
Lucas Yudi <lucas.yudi7@gmail.com>
2022-07-08 00:28:00 -0300

    usando user secrets

--- Sistema-ERP/Program.cs
+++ Sistema-ERP/Program.cs
@@ -21,6 +21,8 @@ namespace Sistema_ERP
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .ConfigureAppConfiguration((context, builder) =>
+            builder.AddUserSecrets<Program>());
     }
 }
